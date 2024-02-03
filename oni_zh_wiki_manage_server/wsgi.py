e6894fa3b54bfc782bfdf4870e767aa4e4aec37b
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-02-03 13:37:17 +0800

    init

---
+++ oni_zh_wiki_manage_server/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for oni_zh_wiki_manage_server project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oni_zh_wiki_manage_server.settings')
+
+application = get_wsgi_application()
