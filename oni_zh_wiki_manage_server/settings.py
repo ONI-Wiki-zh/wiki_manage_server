e45cbf0011a3e2e47a515e1f2bd547b289000c68
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-02-04 17:33:35 +0800

    处理跨域

--- oni_zh_wiki_manage_server/settings.py
+++ oni_zh_wiki_manage_server/settings.py
@@ -50,11 +50,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
 ]
 # 支持跨域配置开始
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-
+CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost',
+)
 ROOT_URLCONF = 'oni_zh_wiki_manage_server.urls'
 
 TEMPLATES = [
